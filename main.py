8d8c66f88fe9f980c47e536907ba4a64be9ed0e3
iigilikov <iigilikov_a@iuca.kg>
2019-12-05 12:00:16 +0000

    123

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import csv
-points = 0
-print("Добро пожаловать! На вопросы отвечайте строго как в вариантах ответа, то есть а) или b)")
-user = input("вы готовы к тесту?")
-
-def start():
-  with open("1.csv", "r", newline="") as file:
-      read = csv.reader(file)
-      for row in read:
-        print(row[0])
-        print(row[1], row[2], row[3])
-        i = input("")
-        if i == row[4]:
-          bal += 5
-          print("true")
-        elif i != row[4]:
-          print("false")
+bal = 0
+print("Добро пожаловать! На вопросы отвечайте строго как в вариантах ответа, то есть а или b")
+with open("1.csv", "r", newline="") as file:
+  read = csv.reader(file)
+  for row in read:
+      print(row[0])
+      print(row[1], row[2], row[3])
+      i = input("")
+      if i == row[4]:
+        bal += 5
+        print("true")
+      elif i != row[4]:
+        print("false")
   print("Вы набрали " + str(bal) + " баллов")
   if bal <= 60:
       print("Ваша оценка F")
@@ -35,12 +32,7 @@ def start():
   elif bal <= 100:
     print("Ваша оценка A")
 
-if user == "да":
-  start()
-else:
-  user = input("Напишите когда будете готовы!(да): ")
-  if user == 'да':
-    start()   
+
  
     
 
